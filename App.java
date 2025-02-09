2872729dfc39bb959c53ef8736ca714b84fe5892
Nathan Magee <mageenp@gmail.com>
2025-02-08 20:44:20 -0500

    first changes

--- App.java
+++ App.java
@@ -1,12 +1,16 @@
 package week05;
 
+//6
 public class App {
     public static void main(String[] args) {
 
+        //6a
+        AsteriskLogger thing = new AsteriskLogger();
+        SpacedLogger thing2 = new SpacedLogger();
+
+        //6b
         String word = "TESTING";
         String word2 = "CHECK";
-
-        AsteriskLogger thing = new AsteriskLogger();
 
         thing.log(word);
         thing.log(word2);
@@ -14,14 +18,11 @@ public class App {
         thing.error(word);
         thing.error(word2);
 
-        SpacedLogger thing2 = new SpacedLogger();
-
         thing2.log(word);
         thing2.log(word2);
 
         thing2.error(word);
         thing2.error(word2);
-
 
     }
 }
